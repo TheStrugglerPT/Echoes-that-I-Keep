59af79c8132be004fae164e1b66da6243f3c248d
TheStrugglerPT <nunombrodrigues95@gmail.com>
2026-06-04 11:16:56 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-/* ---------- ACCENT COLORS ---------- */
-document.querySelectorAll(".post").forEach(post => {
-  const accent = post.dataset.accent;
-  if (accent) {
-    post.style.setProperty("--accent", accent);
-  }
-});
